6095fe5e2f252f677db6f5d2f0b9966542a8fea9
IsidoreTam <59669931+IsidoreTam@users.noreply.github.com>
2021-02-06 19:19:56 -0700

    A bunch of minor changes for today.

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import AlphaVantageModel from './models/alpha-vantage'
+import SearchView from './views/search-view'
+import SearchController from './controllers/search-controller'
+import ResultsView from './views/results-view'
+ 
+window.addEventListener('load', function(e){
+    const apikey = 'IM93V6X6OXWLN65O'
+})
--- src/js/models/alpha-vantage.js
+++ src/js/models/alpha-vantage.js
@@ -1,3 +1,9 @@
 function AlphaVantageModel(){
-    this.apiBaseUrl = 'https://www.alphavantage.co/'
+    this.apiBaseUrl = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol='
+
+    this.query= async function(url){
+        const req = await fetch(url);
+        const res = await req.json();
+        return res
+     }
 }
