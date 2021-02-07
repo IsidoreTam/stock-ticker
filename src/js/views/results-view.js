906aeb9dac31ea686ff9503d5956e36f346a852b
IsidoreTam <59669931+IsidoreTam@users.noreply.github.com>
2021-02-07 12:39:16 -0700

    Finished results-view

--- src/js/views/results-view.js
+++ src/js/views/results-view.js
@@ -1,7 +1,18 @@
 import ejs from ejs
 
+const stockView=`
+<aside class ="stock">
+    <header><h3 class="symbol"><%= stock.symbol %></h3></header>
 
-
+    <ul class = "details">
+        <li>Name: <span><%= stock.name %></span> </li>
+        <li>Price: <span><%= stock.price %></span> </li>
+        <li>Currency: <span><%= stock.currency %></span> </li>
+        <li>Exchange: <span><%= stock.exchange %></span> </li>
+        <li>Country: <span><%= stock.country %></span>  </li>
+        <li>Date: <span><%= stock.date %></span> </li>
+    </ul>
+</aside>`;
 const noResultsView = `
 <aside class="error">
   <header>
@@ -9,3 +20,29 @@ const noResultsView = `
  <header>
 </aside>
 `;
+
+function ResultsView(viewId) {
+  this.container = document.querySelector(viewId);
+
+  this.configUI = function (stock) {
+    const elem = ejs.render(stockview, { stock });
+    this.container.insertAdjacentHTML("afterbegin", elem);
+  };
+
+  this.renderStocks = function (stocks){
+    const elem;
+    // if there are no people in the results
+    this.removeChildElements();
+    if (stocks.results.length === 0) {
+      elem = ejs.render(noResultsView);
+      this.container.insertAdjacentHTML("afterbegin", elem);
+    }
+    // search returns results
+    else if (stocks.results !== 0){
+      stocks.results.forEach((stock) => {
+        elem = ejs.render(stockView, { stock });
+      });
+      this.container.insertAdjacentHTML("afterbegin", elem);
+    }
+  }
+}
